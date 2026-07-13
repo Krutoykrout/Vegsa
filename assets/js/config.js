7f32ea53a912bb19fcf6925a349d698837e1dce1
Krutoykrout <krout00@mail.ru>
2026-07-14 02:22:50 +0700

    Fix catalog buttons and card alignment

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+window.VEGAS_CONFIG = {
+  phoneDisplay: "8 967 050-20-20",
+  phoneHref: "+79670502020",
+  whatsapp: "https://wa.me/79670502020",
+  address: "Москва, ул. Краснобогатырская, дом 2 стр. 16, подъезд 2, этаж 3, офис 7"
+};
